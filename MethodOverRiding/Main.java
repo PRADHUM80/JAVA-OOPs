ee02ec25d394f9c9ec48883607d691fced53793f
Pradhum80 <kumarroney154@gmail.com>
2026-02-05 00:47:11 +0530

    Method : Override

---
+++ MethodOverRiding/Main.java
@@ -0,0 +1,45 @@
+package MethodOverRiding;
+
+// Real Life Example
+//  Bank Interest
+// Parent: getInterest()
+// Child: SBI, HDFC different interest logic
+
+//  Parent Class Ke Method KO Child Class Mein Fir se Likhna.
+
+// Method Name Same
+// Parameter Same
+// Inheritence Required
+// Retrun type Same
+// Access Modifier Not Weak
+// Static , final, Private, MEthod Override Nhe Hote Hain..
+
+class Bank {
+    public void RateOfInterest() {
+        System.out.println("Generally rate of interest is 5%");
+    }
+}
+
+class SBI extends Bank {
+    @Override
+    public void RateOfInterest() {
+        System.out.println("Rate of Interest for SBI is 6.5%");
+    }
+}
+
+class PNB extends Bank {
+    @Override
+    public void RateOfInterest() {
+        System.out.println("Rate of Interest for PNB is 6%");
+    }
+}
+
+ public class Main {
+
+    public static void main(String[] args) {
+        Bank obj = new Bank();
+
+        SBI obj1 = new SBI();
+    }
+    
+}
