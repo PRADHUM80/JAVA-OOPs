cd4534873442e63d2a97bf2a4c37106480bff80c
Pradhum80 <kumarroney154@gmail.com>
2026-02-06 23:18:30 +0530

    Abstract : Class

--- Abstract/Main.java
+++ Abstract/Main.java
@@ -1,5 +1,31 @@
 package Abstract;
 
-public class Main {
+// Abstract is used to hide details and show only important information.
+// Accecc:- 1. Abstract class,  2. Interface
+
+
+
+abstract class Car{
+// abstract Method
+    abstract public void fueltype();
+
+    // normal Method
+    public void color() {
+        System.out.println("Black Color");
+    }
+}
+
+class Tata extends Car{
     
+    public void fueltype() {
+        System.out.println("Diesel");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Tata nexon = new Tata();
+        nexon.fueltype();
+        nexon.color();
+    }
 }
--- Inheritence/ConstructorSuper.java
+++ Inheritence/ConstructorSuper.java
@@ -1,26 +1,23 @@
 package Inheritence;
 
-class Animal {
-    // constructor..
-    Animal()
-    {
-        System.out.println("Animal is Created..");
-    }
-}
-
-class Dog extends Animal{
-    // Constructor
-    Dog()
-    {
-        super();
-        System.out.println("Dog is created");
-    }
-}
-
-
 public class ConstructorSuper {
     public static void main(String[] args) {
         Dog d = new Dog();
     }
-    
+
+}
+
+class Animal {
+    // constructor..
+    Animal() {
+        System.out.println("Animal is Created..");
+    }
+}
+
+class Dog extends Animal {
+    // Constructor
+    Dog() {
+        super();
+        System.out.println("Dog is created");
+    }
 }
